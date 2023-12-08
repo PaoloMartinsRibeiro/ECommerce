59569242451674a88e62431d265ec9f12aa33a56
PaoloMartinsRibeiro <119572269+PaoloMartinsRibeiro@users.noreply.github.com>
2023-12-08 08:46:57 +0100

    feat(cors and xss): adding new packages

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,16 @@ import Produits  from './routes/produits'
 import Utilisateurs  from './routes/utilisateurs'
 import Commandes from './routes/commandes'
 import passport from './middleware/passport'
+import helmet from 'helmet'
+import cors from 'cors'
 
 const app = express() 
 const port = 3030
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.use(cors())
+app.use(helmet())
 
 app.use('/utilisateurs', Utilisateurs)
 app.use('/produits', Produits)
